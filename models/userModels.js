444a4d67b200451feaadc9ae2ec5688b15ba1919
Pratishtha19 <pratishtha1057@chitkara.edu.in>
2024-01-13 21:59:26 +0530

    first commit

---
+++ models/userModels.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+      type:String,
+      required:[true,'name is required']
+    },
+    email:{
+      type:String,
+      required:[true,'email is required']
+    },
+    password:{
+      type:String,
+      required:[true,'password is required']
+    },
+    role: {
+      type: String,
+      enum: ['student', 'teacher'],
+      required: [true, 'role is required'],
+    },
+})
+
+const userModel = mongoose.model('users',userSchema)
+
+module.exports = userModel
